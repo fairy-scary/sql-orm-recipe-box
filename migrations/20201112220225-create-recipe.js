8ea58421b11f05fa4b8f79852c7e9e055125df23
fairy-scary <ecoautumn@gmail.com>
2020-11-12 16:10:09 -0600

    at Phase 7

---
+++ migrations/20201112220225-create-recipe.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(200)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        default: true
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        default: true
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipes');
+  }
+};
